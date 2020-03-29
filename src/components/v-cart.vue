25bcb2bcdcb0690019edc885e38952156831dccc
Your Name <tleugazy98.com>
2020-03-29 22:13:11 +0600

    first

---
+++ src/components/v-cart.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="v-cart">
+        <h1>Корзина</h1>
+        <v-cart-item
+               v-for="item in cart_data"
+               :cart_item_data="item"
+               :key="item.article"
+        />
+    </div>
+</template>
+
+<script>
+    import vCartItem from './v-cart-item'
+    export default {
+        name: "v-cart.vue",
+        components:{
+          vCartItem
+        },
+        props:{
+            cart_data:{
+                type:Array,
+                default() {
+                    return [];
+                }
+            }
+        },
+        data(){
+            return{
+
+            }
+        },
+        computed:{
+
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/v-main-wrapper.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="v-main-wrapper">
+        <p>{{title}}</p>
+        <v-cart
+                v-if="CART.length"
+                :cart_data="CART"
+        />
+        <v-catalog/>
+
+    </div>
+</template>
+<script>
+    import vCatalog from './v-catalog'
+    import vCart from "./v-cart"
+    import {mapGetters} from  "vuex"
+    export default{
+        name:"v-main-wrapper",
+        components:{
+            vCatalog,
+            vCart
+        },
+        props:{},
+        data(){
+            return{
+                title:"Онлайн магазин"
+            }
+        },
+        computed:{
+            ...mapGetters([
+                "CART"
+            ])
+        },
+        methods:{
+
+        },
+        watch:{
+
+        },
+        mounted(){
+            console.log("Hello from mounted")
+        },
+    }
+</script>
+
+<style scoped>
+    .v-main-wrapper{
+
+        max-width: 900px;
+
+        margin:0 auto;
+    }
+</style>
